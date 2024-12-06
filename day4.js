32d8b1bc327c6ddb8d85911fea5be2d6537cf275
Henri Tuan <henri.tuan77@gmail.com>
2024-12-07 00:15:53 +0100

    WIP day4

---
+++ day4.js
@@ -0,0 +1,16 @@
+const countXmas = (input) => {
+  console.log(input.replace("\n", ""));
+};
+
+const input1 = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`;
+
+countXmas(input1);
